4f21fe79746630fe2b7b3c01904ae6d3f2d8165f
joelmaridas <joel254@revature.net>
2022-10-31 16:59:07 -0500

    pushed monday CodingDojo

--- Coding-Dojo-single-and-ready-to-mingle/src/com/revature/Single_and_ready_to_mingle.java
+++ Coding-Dojo-single-and-ready-to-mingle/src/com/revature/Single_and_ready_to_mingle.java
@@ -3,31 +3,46 @@ package com.revature;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import org.hamcrest.Matcher;
 
 public class Single_and_ready_to_mingle {
-	public static void find_singles( ArrayList <Integer> arr) {
-		HashSet<Integer> num=new HashSet<Integer>();
+	public static String find_singles( ArrayList <String> arr) {
+		HashSet<Integer> num=new HashSet<Integer>(); 
+		for (int j=0;j<arr.size();j++) {
+			if(!arr.get(j).matches("[0-9]+")) {
+				//System.out.println("Error");
+			return("Error");
+			}
+			Integer a=Integer.parseInt(arr.get(j));
 		
 
-		for (int j=0;j<arr.size();j++) {
-			if(num.contains(arr.get(j))) {
-				num.remove(arr.remove(j));
+			if(!num.contains(a)) {
+				num.add(a);
+			//System.out.println("Add="+num);
 			}
 			else {
-				num.add(arr.get(j));
+				num.remove(a);
+				//System.out.println("Remove="+num);
 			}
 		}
-		System.out.println(num);
+		return(String.valueOf(num));
+		//System.out.println(num);
 		}
 		public static void main(String[] args){
 			Scanner in=new Scanner(System.in);
-			 ArrayList<Integer> arr=new ArrayList<Integer>();
+			 ArrayList<String> arr=new ArrayList<String>();
 			 System.out.println("Enter the length");
 			 int n=in.nextInt();
-			for (int i=0;i<n;i++) {
-				System.out.println("Enter Number");
-				arr.add(in.nextInt());
+			 in.nextLine();
+			try {
+				for (int i=0;i<n;i++) {
+					System.out.println("Enter Number");
+					arr.add(in.nextLine());
+				}
 			}
+			catch(Exception e) {System.out.println(e);}
 			find_singles(arr);
 			
 		}
--- Coding-Dojo-single-and-ready-to-mingle/src/com/revature/TestCases.java
+++ Coding-Dojo-single-and-ready-to-mingle/src/com/revature/TestCases.java
@@ -1,14 +1,61 @@
 package com.revature;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class TestCases {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+Single_and_ready_to_mingle num;
+ArrayList<String> arr;
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+@Before
+public void ele()
+{
+	arr=new ArrayList<String>();	
+	 arr.add("a");
+	 arr.add("1");
+	 arr.add("2");
+	 arr.add("3");
+	 arr.add("4");
 }
+     @Test
+    void Empty() {
+    num=new Single_and_ready_to_mingle();
+ 
+    }
+     @Test
+     void singles() {
+    	 
+     }
+     @Test
+     void doubles() {
+    	 
+     }
+     @Test
+     void thriples() {
+    	 
+     }
+     @Test
+    void exception() {
+    	 arr=new ArrayList<String>();	
+    	 arr.add("a");
+    	 arr.add("1");
+    	 arr.add("2");
+    	 arr.add("3");
+    	 arr.add("4");
+    	num=new Single_and_ready_to_mingle();
+    	System.out.println(arr);
+    	Single_and_ready_to_mingle.find_singles(arr);
+ 
+    	assertEquals(Single_and_ready_to_mingle.find_singles(arr), "Error");
+    	
+    }	
+    }
+    
+
